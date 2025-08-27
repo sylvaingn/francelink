e6030b5c61fe5e9aad4fff6a3717a07d998ed40e
Sylvain <sylvain.gounon@francelink.fr>
2025-08-27 18:41:20 +0200

    sto

--- wp-content/themes/contentify/template-parts/loop/card-faq.php
+++ wp-content/themes/contentify/template-parts/loop/card-faq.php
@@ -1,13 +1,17 @@
 <?php
-$terms = get_the_terms(get_the_ID(), 'type-faq');
+$post_id = get_the_ID(); // ok en boucle principale
+$terms_slugs = [];
 
-$terms_slugs = array_map(function ($term) {
-    return $term->slug;
-}, $terms);
+$terms = get_the_terms( $post_id, 'type-faq' );
 
+if ( $terms && ! is_wp_error($terms) ) {
+    // Plus simple et sûr : extrait directement les slugs
+    $terms_slugs = wp_list_pluck( $terms, 'slug' );
+}
 ?>
-<div class="faq-card stretched-container" data-post-terms="<?php echo implode(',', $terms_slugs); ?>">
+<div class="faq-card stretched-container"
+     data-post-terms="<?php echo esc_attr( implode(',', $terms_slugs) ); ?>">
     <div class="faq-card--content">
-        <a href="<?php the_permalink(); ?>" class="title subtitle stretched-link"><?php the_title(); ?></a>
+        <a href="<?php the_permalink(); ?>" class="title stretched-link"><?php the_title(); ?></a>
     </div>
 </div>
