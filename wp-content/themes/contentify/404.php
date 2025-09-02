2b27b9670bb8a6234e7bfd8553dfcc919424aab9
Sylvain Gounon <bonjour@sylvaingounon.com>
2025-09-02 10:19:36 +0200

    404

--- wp-content/themes/contentify/404.php
+++ wp-content/themes/contentify/404.php
@@ -3,14 +3,17 @@
     <div class="page-404 large-content">
         <div class="container">
             <div class="page-404--wrapper">
-                <h1 class="title menu-title"><?php echo __('404', TEXT_DOMAIN); ?></h1>
+                <h1 class="title big-title"><?php echo __('404', TEXT_DOMAIN); ?></h1>
                 <p class="text-center"><?php echo __('Désolé mais cette page n\'est pas disponible...', TEXT_DOMAIN); ?></p>
 
                 <div class="btn--wrapper">
                     <a href="<?php echo get_site_url(); ?>"
-                       class="btn"><?php echo __('Revenir à la page d\'accueil', TEXT_DOMAIN); ?></a>
-                    <a href="<?php echo get_permalink(BLOG_PAGE_ID); ?>"
-                       class="btn btn-secondary"><?php echo __('Voir notre actualité', TEXT_DOMAIN); ?></a>
+                       class="btn btn-primary"><?php echo __('Revenir à la page d\'accueil', TEXT_DOMAIN); ?></a>
+                    
+                    <?php if ( get_post_type_archive_link('faq' ) ): ?>
+                    <a href="<?php echo get_post_type_archive_link('faq' ); ?>"
+                       class="btn"><?php echo __('Visiter la FAQ', TEXT_DOMAIN); ?></a>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
